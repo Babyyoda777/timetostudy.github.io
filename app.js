af8eaedfdeb984703163a6d16491c337b5c9f76e
babyyoda777 <azlan777@icloud.com>
2021-06-02 12:06:17 +0100

    added dark mode

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-var TimerObj = (function(document){
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
-   var Timer;
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
 
-   function start() {
-     Timer = setInterval(myClock, 1000);
-     var c = 5;
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
 
-     function myClock() {
-       document.getElementById("timer").innerHTML = --c;
-       if (c == -1) {
-         clearInterval(Timer);
-         alert("That's it! Study time over!");
-       }
-     }
-   }
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else
+  {document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
 
-   function end() {
-       clearInterval(Timer)
-   }
+toggleSwitch.addEventListener('change', switchTheme, false);
+//# sourceURL=pen.js
 
-   return {start:start, end:end};
- })(document);
+
+var TimerObj = (function(document) {
+
+  var Timer;
+
+  function start() {
+    Timer = setInterval(myClock, 1000);
+    var c = 5;
+
+    function myClock() {
+      document.getElementById("timer").innerHTML = --c;
+      if (c == -1) {
+        clearInterval(Timer);
+        alert("That's it! Study time over!");
+      }
+    }
+  }
+
+  function end() {
+    clearInterval(Timer)
+  }
+
+  return {
+    start: start,
+    end: end
+  };
+})(document);
